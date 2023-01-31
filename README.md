7351d6fa4fe88afc82dd149af88e7a637f11678e
Kieran Collins <kiecol@protonmail.com>
2023-01-31 16:05:23 +0000

    Add map challenge to map.js and update readme to reflect same

--- README.md
+++ README.md
@@ -23,6 +23,16 @@ The code examples in the "map.js" file show the following variations of using th
 
 In addition to transforming arrays with numbers, the code also demonstrates how to transform arrays with objects.
 
+### The Map Challenge
+
+You will use destructuring assignment to create two variables and you will assign them values from calling the map method on the students array. The function provided to the map method can either be created within the map method or outside and passed in but in either case, it should use an arrow function. 
+The map method should return an array for each item in the students array and this new array should contain two items the items name value and the items results value.
+- Create variables named john and rest using the destructuring assignment
+- Assign the variables values from calling the map method on the students array
+- Within the map method either use an arrow function or provide a function created outside of the map method (note this function should also be an arrow function)
+- Log out the variable named: john to see its value
+- Log out the variable named: rest to see its value
+
 ### map.js Terminal Output
 
     gitpod /workspace/js-map-filter-reduce (main) $ node map.js
@@ -32,4 +42,12 @@ In addition to transforming arrays with numbers, the code also demonstrates how 
     Simplified w/ map()  [ 2, 4, 6, 8, 10 ]
     Simplfied w/ map() + arrow function  [ 2, 4, 6, 8, 10 ]
     Map() with objects  [ [ 'Mark', 1 ], [ 'Ariel', 2 ], [ 'Jason', 3 ] ]
+    -------------------------------------------------------------------
+    The Map Challenge
+    john: [ 'John', { maths: 90, english: 75, cad: 87 } ]
+    rest: [
+    [ 'Emily', { science: 93, english: 80, art: 95 } ],
+    [ 'Adam', { science: 93, maths: 77, art: 95 } ],
+    [ 'Fran', { science: 93, english: 87, art: 95 } ]
+    ]
 
--- map.js
+++ map.js
@@ -28,7 +28,7 @@ const double = nums.map(num => num * 2);
 console.log("Simplfied w/ map() + arrow function ", double);
 
 // With objects:
-const students = [
+const students1 = [
   {
     id: 1,
     name: 'Mark',
@@ -49,5 +49,59 @@ const students = [
   },
 ];
 
-const studentsWithIds = students.map(student => [student.name, student.id]);
+const studentsWithIds = students1.map(student => [student.name, student.id]);
 console.log("Map() with objects ", studentsWithIds)
+
+// The Map Challenge
+
+// Steps:
+// You will use destructuring assignment to create two variables and you will assign them values from calling the map method on the students array. The function provided to the map method can either be created within the map method or outside and passed in but in either case, it should use an arrow function. 
+// The map method should return an array for each item in the students array and this new array should contain two items the items name value and the items results value.
+// Create variables named john and rest using the destructuring assignment
+// Assign the variables values from calling the map method on the students array
+// Within the map method either use an arrow function or provide a function created outside of the map method (note this function should also be an arrow function)
+// Log out the variable named: john to see its value
+// Log out the variable named: rest to see its value
+
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, maths: 77, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+// Solution: 
+
+// Destructuring assignment to create variables `john` and `rest`
+let [john, ...rest] = students.map(student => [student.name, student.results]);
+
+// Logging the divider and heading
+console.log("-------------------------------------------------------------------");
+console.log("The Map Challenge")
+
+// Logging the value of `john`
+console.log("john:", john);
+
+// Logging the value of `rest`
+console.log("rest:", rest);
