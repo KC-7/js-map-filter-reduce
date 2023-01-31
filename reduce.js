ab69a4e384ce8821096eb09de7e85a58cfa25d70
Kieran Collins <kiecol@protonmail.com>
2023-01-31 17:27:22 +0000

    Add reduce method code for suming an array of numbers (v1 - v4)

--- reduce.js
+++ reduce.js
@@ -6,8 +6,50 @@
  */
 
 
-// Summing an array of numbers:
+// --------------------------------------------------- Summing an array of numbers:
 
+const nums = [0, 1, 2, 3, 4];
+
+// acc stands for accumulator (value that will be returned from reduce method)
+// curr stands for current value
+
+// Ver 1 - Provides Answer Only
+// let sum = nums.reduce((acc, curr) => acc + curr);
+// console.log(sum)
+
+// Ver 2 - Provides acc, curr and tot for each additional value
+// let sum = nums.reduce((acc, curr) => {
+//     console.log(
+//         "Accumulator:", acc,
+//         "Current Value:", curr,
+//         "Total:", acc + curr
+//     );
+//     return acc + curr;
+// });
+// console.log("Final Value:", sum)
+
+/**
+ * Ver 3:
+ * Provides acc, curr and tot for each additional value. 
+ * Starting from the 0 value. Note: The 0 value can be 
+ * changed to change the starting point for the accumulator. 
+ */
+
+let sum = nums.reduce((acc, curr) => {
+    console.log(
+        "Accumulator:", acc,
+        "Current Value:", curr,
+        "Total:", acc + curr
+    );
+    return acc + curr;
+}, 0);
+console.log("Final Value:", sum)
+
+// Ver 4 - Provides Answer Only (with ", 0" added to end of code to prevent issues)
+// let sum = nums.reduce((acc, curr) => acc + curr, 0);
+// console.log(sum)
+
+// --------------------------------------------------- Totaling a specific object property
 
 const teamMembers = [
     {
@@ -31,9 +73,9 @@ const teamMembers = [
       yrsExperience: 3
     }
   ];
+
+// Totaling a specific object property
+
+
   
-  // Totaling a specific object property
-  
-  
-  // Grouping by a property, and totaling it too
-  
+// --------------------------------------------------- Grouping by a property, and totaling it too
